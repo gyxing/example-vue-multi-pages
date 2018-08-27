7540771a073d83d4086cd81dfadadcf5cb2d40e4
gyxing <172176234@qq.com>
2018-08-27 21:15:06 +0800

    ...

--- build/config.js
+++ build/config.js
@@ -3,7 +3,7 @@ module.exports = {
     dev: {
         outputPath: path.resolve(__dirname, '../static'),
         outputPublicPath: '/',
-        port: 8070
+        port: 8090
     },
     prod: {
         outputPath: path.resolve(__dirname, '../dist/assets'),
